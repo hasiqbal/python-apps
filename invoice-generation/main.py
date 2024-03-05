7e58a6b8ca8a28817be1540a41c87a2a106b2f74
hasiqbal <ha.s.iqbal@outlook.com>
2024-03-05 13:59:50 +0000

    final

--- invoice-generation/main.py
+++ invoice-generation/main.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 filepaths = glob.glob('invoice-generation/invoices/*.xlsx')
 
+# to read file paths
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name='Sheet 1')
 
@@ -22,5 +23,37 @@ for filepath in filepaths:
     pdf.set_font(family='Times', size=16, style='B')
     pdf.cell(w=50, h=8, txt= f'date:{date}', ln=1)
 
+   # table column names
+    columns = list(df.columns)
+    columns = [item.replace('_', ' ').title() for item in columns]
+    pdf.set_font(family='Times', size=10, style= 'B')
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], border=1)
+    pdf.cell(w=35, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
 
-    pdf.output(f'invoice-generation/invoices{filename}.pdf')
+    
+   # table rows     
+    for index, row in df.iterrows():
+        pdf.set_font(family='Times', size=10, style= 'B')
+        pdf.cell(w=30, h=8, txt= f'{row["product_id"]}', border=1),
+        pdf.cell(w=70, h=8, txt= f'{row["product_name"]}', border=1)
+        pdf.cell(w=35, h=8, txt= f'{row["amount_purchased"]}', border=1)
+        pdf.cell(w=30, h=8, txt= f'{row["price_per_unit"]}', border=1)
+        pdf.cell(w=30, h=8, txt= f'{row["total_price"]}', border=1, ln=1)
+       
+total_sum = df['total_price'].sum()       
+pdf.set_font(family='Times', size=10, style= 'B')
+pdf.cell(w=30, h=8, txt= '', border=1),
+pdf.cell(w=70, h=8, txt= '', border=1)
+pdf.cell(w=35, h=8, txt= '', border=1)
+pdf.cell(w=30, h=8, txt= '', border=1)
+pdf.cell(w=30, h=8, txt= str(total_sum), border=1, ln=1)
+
+
+pdf.set_font(family='Times', size=10, style= 'B')
+pdf.cell(w=30, h=8, txt=f'the the total price is {total_sum}', ln=1)
+
+pdf.output(f'invoice-generation/invoices{filename}.pdf')
+
