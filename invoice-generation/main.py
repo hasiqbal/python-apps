caf8182709c4f736028f8a569c4502ccbc9141d4
hasiqbal <ha.s.iqbal@outlook.com>
2024-03-05 12:14:37 +0000

    pdf output with invoice number

--- invoice-generation/main.py
+++ invoice-generation/main.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
-filepaths = glob.glob('invoices/*.xlsx')
+filepaths = glob.glob('invoice-generation/invoices/*.xlsx')
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name='Sheet 1')
-    print(df)
+    pdf = FPDF(orientation='L', unit='mm', format='A4')
+    pdf.set_font(family='Times', size=16, style='B')
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoiceNumber = filename.split('-')[0]
+    pdf.cell(w=50, h=8, txt=f'Invoice Number:{invoiceNumber}')
+    pdf.output(f'invoice-generation/invoices{filename}.pdf')
