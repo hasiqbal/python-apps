16cc7b10af3c75cc767ff099fdc953b7550ca4b5
hasiqbal <ha.s.iqbal@outlook.com>
2024-03-06 11:47:19 +0000

    limited article number to 20 and in english and made the topic parameter in the url dynamic

--- news-email-api/api.py
+++ news-email-api/api.py
@@ -1,21 +1,22 @@
 import requests
 from send_email import send_email
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-02-06&sortBy=publishedAt&apiKey=99e685768c5945748caa5bc63ab676fd"
+topic = "tesla"
+url = "https://newsapi.org/v2/everything?q={topic}&from=2024-02-06&sortBy=publishedAt&language=en&apiKey=99e685768c5945748caa5bc63ab676fd"
 
 def get_url():
     request = requests.get(url)
     content = request.json()    
-    articles = content["articles"]
+    articles = content["articles"][:20]
     # List of tuples containing URL and title
-    body = [(article["url"], article["title"]) for article in articles]
+    body = [(article["url"], article["title"], article["author"]) for article in articles]
     return body
 
 # Get list of URLs and titles
 urls_and_titles = get_url()
 
 # Construct email body
-email_body = "\n".join([f"{title}: {url}\n" for url, title in urls_and_titles])
+email_body = "\n".join([f"{title}:\n {url}\n The article is by: {author}\n" for url, title, author in urls_and_titles])
 
 
 # Send email
