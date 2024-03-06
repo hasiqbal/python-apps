8f413a54bb3e8dbec4dada63153cc6ae79e1a10e
hasiqbal <ha.s.iqbal@outlook.com>
2024-03-06 11:32:31 +0000

    added titles and new lines

--- news-email-api/api.py
+++ news-email-api/api.py
@@ -1,18 +1,24 @@
 import requests
 from send_email import send_email
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-02-05&sortBy=publishedAt&apiKey=99e685768c5945748caa5bc63ab676fd"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2024-02-06&sortBy=publishedAt&apiKey=99e685768c5945748caa5bc63ab676fd"
 
 def get_url():
-
     request = requests.get(url)
-    content = request.json()
+    content = request.json()    
     articles = content["articles"]
-    body = [article["url"] for article in articles]
+    # List of tuples containing URL and title
+    body = [(article["url"], article["title"]) for article in articles]
     return body
-    
-urls = get_url()
-email = '\n'.join(urls)  
 
-send_email(email) 
+# Get list of URLs and titles
+urls_and_titles = get_url()
+
+# Construct email body
+email_body = "\n".join([f"{title}: {url}\n" for url, title in urls_and_titles])
+
+
+# Send email
+send_email(email_body)
+
 
